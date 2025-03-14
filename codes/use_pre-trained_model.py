9a176d5d8abe114cc6c13fc9072e953533107e62
Aryan Badiee <aryanbadiee333@gmail.com>
2025-03-14 05:47:04 +0100

    Uses the trained model for classification

---
+++ codes/use_pre-trained_model.py
@@ -0,0 +1,15 @@
+from website_classification import WebsiteCategorization
+
+
+if __name__ == "__main__":
+    # Loads the model:
+    cls = WebsiteCategorization.load("../model/pre-trained_model.ab3")
+
+    # Predicts the category of the domain and the confidence score:
+    sample_domain = "https://javacup.ir"
+    predicted_category, conf = cls.predict(sample_domain)
+    print(
+        f"The category of {sample_domain} is {predicted_category}",
+        f"The confidence score is {conf}",
+        sep='\n'
+    )
