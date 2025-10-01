2395a852ab32e54d0b8b126e7296c0af3c3a04d4
Aryan Badiee <aryanbadiee333@gmail.com>
2025-10-01 04:28:30 +0200

    Support for 'nonlinearSVC' model

--- codes/website_classification.py
+++ codes/website_classification.py
@@ -8,13 +8,13 @@ import numpy as np
 import requests
 from bs4 import BeautifulSoup
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.svm import LinearSVC
+from sklearn.svm import LinearSVC, SVC
 
 
 class WebsiteCategorization:
     """ This class provides facilities for training and predicting the category of websites """
 
-    def __init__(self, unique_categories: Iterable, /):
+    def __init__(self, unique_categories: Iterable, model: str = "linear", /):
         # Category to ID:
         cat2id = dict[str, int]()
         for i, category in enumerate(unique_categories):
@@ -27,6 +27,22 @@ class WebsiteCategorization:
 
         self.cat2id = cat2id
         self.id2cat = id2cat
+
+        # TF-IDF (term frequency-inverse document frequency):
+        self.vectorizer = TfidfVectorizer(
+            strip_accents="unicode",
+            analyzer="word",
+            max_df=.25, min_df=.0,  # [0.0, 0.25] to remove stopwords!
+            # max_features=256
+        )
+
+        # Model:
+        if model == "linearSVC":
+            self.model = LinearSVC(dual="auto")  # Linear
+        elif model == "nonlinearSVC":
+            self.model = SVC(kernel="poly", degree=3)  # Polynomial of degree 3
+        else:
+            raise TypeError("Invalid model!")
 
     def train(self, train_data: dict[str, list], /) -> None:
         """ Trains a model """
@@ -43,20 +59,10 @@ class WebsiteCategorization:
             train_ids.append(self.cat2id[category])
 
         # Creates feature vectors using TF-IDF vectorization:
-        vectorizer = TfidfVectorizer(
-            strip_accents="unicode",
-            analyzer="word",
-            max_df=.25, min_df=.0,  # [0.0, 0.25] to remove stopwords!
-            # max_features=256
-        )
-        train_vectors = vectorizer.fit_transform(train_texts)
+        train_vectors = self.vectorizer.fit_transform(train_texts)
 
-        # Trains a linear support vector classifier:
-        classifier = LinearSVC(dual='auto')
-        classifier.fit(train_vectors, train_ids)
-
-        self.vectorizer = vectorizer
-        self.classifier = classifier
+        # Trains the model:
+        self.model.fit(train_vectors, train_ids)
 
     def predict(self, domain: str, /) -> tuple[str, int]:
         """ Predicts the category of the domain and the confidence score """
@@ -64,10 +70,10 @@ class WebsiteCategorization:
         text = WebsiteCategorization.extract_text(domain)
 
         text_vector = self.vectorizer.transform([text])
-        scores = self.classifier.decision_function(text_vector)[0]
-        category_id = np.argmax(scores)
+        confs = self.model.decision_function(text_vector)[0]
+        category_id = np.argmax(confs)
 
-        return self.id2cat[category_id], scores[category_id]
+        return self.id2cat[category_id], confs[category_id]
 
     def save(self, model_path: AnyStr, /) -> None:
         """ Saves the model """
@@ -107,17 +113,10 @@ if __name__ == "__main__":
                 data[category_].append(domain_)
 
     # Builds and trains the model:
-    cls = WebsiteCategorization(data.keys())
+    cls = WebsiteCategorization(data.keys(), "nonlinearSVC")
     cls.train(data)
 
     # Saves the model:
     cls.save("../model/pre-trained_model.ab3")
 
-    # Predicts the category of the domain and the confidence score:
-    sample_domain = "https://testhelper.ir/"
-    predicted_category, conf = cls.predict(sample_domain)
-    print(
-        f"The category of {sample_domain} is {predicted_category}",
-        f"The confidence score is {conf}",
-        sep='\n'
-    )
+    print("The model has been saved.")
