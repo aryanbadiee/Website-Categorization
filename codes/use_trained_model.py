acf2dc3bfed06d956c019a91f8abd7e2b6948fc5
Aryan Badiee <aryanbadiee333@gmail.com>
2025-10-01 04:24:47 +0200

    Better appearance for comments!

--- codes/use_trained_model.py
+++ codes/use_trained_model.py
@@ -2,14 +2,12 @@ from website_classification import WebsiteCategorization
 
 
 if __name__ == "__main__":
-    # Loads the model:
+    # Loads the model
     cls = WebsiteCategorization.load("../model/pre-trained_model.ab3")
 
-    # Predicts the category of the domain and the confidence score:
-    sample_domain = "https://javacup.ir"
+    # Predicts the category of the domain and the confidence score
+    sample_domain = "https://ui.ac.ir/"
     predicted_category, conf = cls.predict(sample_domain)
-    print(
-        f"The category of {sample_domain} is {predicted_category}",
-        f"The confidence score is {conf}",
-        sep='\n'
-    )
+    print(f"The category of {sample_domain} is {predicted_category}",
+          f"The confidence score is {conf:.4f}",
+          sep='\n')
